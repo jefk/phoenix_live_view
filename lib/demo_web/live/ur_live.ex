011244c726cdfbc7fdf024e5f4538537fb28dfcd
jefk <jeff@substantial.com>
2020-01-22 12:29:53 -0800

    new game state

--- lib/demo_web/live/ur_live.ex
+++ lib/demo_web/live/ur_live.ex
@@ -5,31 +5,35 @@ defmodule DemoWeb.UrLive do
 
   @topic "ur"
   @roll_event "event:roll"
+  @initial_state %{
+    current_roll: nil,
+    alice: Enum.map(1..14, fn _ -> false end),
+    bob: Enum.map(1..14, fn _ -> false end)
+  }
 
-  def render(assigns), do: UrView.render("index.html", assigns)
+  def render(%{state: state}) do
+    template_assigns = state |> Map.merge(%{cells: get_cells(state)})
+    UrView.render("index.html", template_assigns)
+  end
 
   def mount(_session, socket) do
     DemoWeb.Endpoint.subscribe(@topic)
-    {:ok, assign(socket, val: nil, cells: initial_cells(), player_a: 0)}
+
+    {:ok, assign(socket, %{state: @initial_state})}
   end
 
-  def handle_event("roll", _, socket) do
-    new_roll = roll()
-    DemoWeb.Endpoint.broadcast_from(self(), @topic, @roll_event, %{val: new_roll})
-    {:noreply, assign(socket, val: new_roll)}
-  end
-
-  def handle_event("move", _, %{assigns: %{val: nil}} = socket) do
-    {:noreply, socket}
+  def handle_event("roll", _, %{assigns: %{state: state}} = socket) do
+    state = state |> roll_update
+    DemoWeb.Endpoint.broadcast_from(self(), @topic, @roll_event, state)
+    {:noreply, assign(socket, %{state: state})}
   end
 
   def handle_event("move", _, socket) do
-    new_player_a = socket.assigns.val + socket.assigns.player_a
-    new_cells = update_for_play(new_player_a)
-    {:noreply, assign(socket, val: nil, player_a: new_player_a, cells: new_cells)}
+    {:noreply, socket}
   end
 
   def handle_info(%{event: @roll_event, payload: state}, socket) do
+    IO.inspect('handle')
     {:noreply, assign(socket, state)}
   end
 
@@ -37,34 +41,21 @@ defmodule DemoWeb.UrLive do
     Enum.sum([Enum.random(0..1), Enum.random(0..1), Enum.random(0..1), Enum.random(0..1)])
   end
 
-  defp initial_cells do
-    Enum.flat_map(1..14, &initial_cell/1)
-  end
-
-  defp initial_cell(index) do
-    case index do
-      x when x in 5..12 ->
-        [%{name: "s#{index}", occupied: ""}]
-
-      _ ->
-        [%{name: "a#{index}", occupied: ""}, %{name: "b#{index}", occupied: ""}]
-    end
-  end
-
-  defp update_for_play(move) do
+  defp get_cells(_state) do
     1..14
-    |> Enum.flat_map(fn
-      index ->
-        case index do
-          x when x in 5..12 ->
-            [%{name: "s#{index}", occupied: if(move == index, do: "a", else: "")}]
+    |> Enum.flat_map(fn index ->
+      case index do
+        x when x in 5..12 ->
+          [%{name: "s#{index}", occupied: ""}]
 
-          _ ->
-            [
-              %{name: "a#{index}", occupied: if(move == index, do: "a", else: "")},
-              %{name: "b#{index}", occupied: ""}
-            ]
-        end
+        _ ->
+          [%{name: "a#{index}", occupied: ""}, %{name: "b#{index}", occupied: ""}]
+      end
     end)
+  end
+
+  defp roll_update(state) do
+    state
+    |> Map.merge(%{current_roll: roll()})
   end
 end
