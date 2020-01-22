4489ea14f4d6a5913d36f90ca9176c7beda0ad52
jefk <speedracer111@gmail.com>
2020-01-21 21:40:05 -0800

    sync roll state

--- lib/demo_web/live/ur_live.ex
+++ lib/demo_web/live/ur_live.ex
@@ -1,13 +1,15 @@
 defmodule DemoWeb.UrLive do
   use Phoenix.LiveView
 
+  @topic "ur"
+
   def render(assigns) do
     ~L"""
     <div>
       <h1>The Royal Game of Ur</h1>
       <%= if @val do %>
       <span><%= @val %></span>
-      <%= end %>
+      <% end %>
       <div>
       <button phx-click="roll">Roll</button>
       </div>
@@ -16,14 +18,21 @@ defmodule DemoWeb.UrLive do
   end
 
   def mount(_session, socket) do
+    DemoWeb.Endpoint.subscribe(@topic)
     {:ok, assign(socket, val: nil)}
   end
 
   def handle_event("roll", _, socket) do
-    {:noreply, update(socket, :val, &roll/1)}
+    new_roll = roll()
+    DemoWeb.Endpoint.broadcast_from(self(), @topic, "roll", %{val: new_roll})
+    {:noreply, assign(socket, val: new_roll)}
   end
 
-  defp roll(_) do
+  def handle_info(%{event: "roll", payload: state}, socket) do
+    {:noreply, assign(socket, state)}
+  end
+
+  defp roll() do
     Enum.sum([Enum.random(0..1), Enum.random(0..1), Enum.random(0..1), Enum.random(0..1)])
   end
 end
