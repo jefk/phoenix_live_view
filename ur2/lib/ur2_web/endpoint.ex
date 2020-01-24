e262e094653a28405a3f858b8b7803d840f1a7cf
jefk <jeff@substantial.com>
2020-01-24 15:06:18 -0800

    heroku

--- ur2/lib/ur2_web/endpoint.ex
+++ ur2/lib/ur2_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule Ur2Web.Endpoint do
   ]
 
   socket "/socket", Ur2Web.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
